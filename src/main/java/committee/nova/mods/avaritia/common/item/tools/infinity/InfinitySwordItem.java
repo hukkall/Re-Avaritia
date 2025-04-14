6b402e456780fef45141bbc29b6b84fbf3a1ac42
cnlimiter <cnlimiter@163.com>
2025-04-14 19:52:27 +0800

    fix(infinitysword): improve entity damage handling and death logic
    - Add custom hurt and die methods to properly handle entity damage and death
    - Implement knockback, damage events, and other effects- Fix issues with player kill credit and score
    - Add support for Wither rose creation on death
    - Optimize code structure and improve readability

--- src/main/java/committee/nova/mods/avaritia/common/item/tools/infinity/InfinitySwordItem.java
+++ src/main/java/committee/nova/mods/avaritia/common/item/tools/infinity/InfinitySwordItem.java
@@ -1,28 +1,45 @@
 package committee.nova.mods.avaritia.common.item.tools.infinity;
 
+import committee.nova.mods.avaritia.Static;
 import committee.nova.mods.avaritia.api.iface.IMultiFunction;
 import committee.nova.mods.avaritia.api.iface.InitEnchantItem;
 import committee.nova.mods.avaritia.common.entity.ImmortalItemEntity;
 import committee.nova.mods.avaritia.init.config.ModConfig;
 import committee.nova.mods.avaritia.init.registry.*;
 import committee.nova.mods.avaritia.util.ToolUtils;
+import net.minecraft.advancements.CriteriaTriggers;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
+import net.minecraft.tags.DamageTypeTags;
+import net.minecraft.tags.EntityTypeTags;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Pose;
 import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
+import net.minecraft.world.entity.boss.wither.WitherBoss;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.Enchantments;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.gameevent.GameEvent;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -43,14 +60,13 @@ public class InfinitySwordItem extends SwordItem implements IMultiFunction, Init
     }
 
     @Override
-    public boolean onLeftClickEntity(ItemStack stack, Player player, Entity victim) {
+    public boolean onLeftClickEntity(ItemStack stack, Player player, Entity entity) {
         var level = player.level();
         var endlessDamage = ModConfig.isSwordAttackEndless.get();
-        if (!level.isClientSide && level instanceof ServerLevel serverLevel) {
+        if (!level.isClientSide && level instanceof ServerLevel serverLevel && entity instanceof LivingEntity victim) {
             var damageSource = player.damageSources().source(ModDamageTypes.INFINITY, victim, player);
             ToolUtils.sweepAttack(serverLevel, player, victim);//横扫
             if (victim instanceof EnderDragon dragon ) {
-                victim.setInvulnerable(false);//取消无敌
                 dragon.hurt(dragon.head, damageSource, endlessDamage ? Float.MAX_VALUE : this.getTier().getAttackDamageBonus());
             } else if (victim instanceof Player pvp) {
                 if (ToolUtils.isInfinite(pvp)) {
@@ -58,72 +74,172 @@ public class InfinitySwordItem extends SwordItem implements IMultiFunction, Init
                     serverLevel.explode(player, pvp.getBlockX(), pvp.getBlockY(), pvp.getBlockZ(), 25.0F, Level.ExplosionInteraction.MOB);
                     return true;//直接返回
                 } else {
-                    victim.setInvulnerable(false);
-                    victim.hurt(damageSource, endlessDamage ? Float.MAX_VALUE : this.getTier().getAttackDamageBonus());
+                    this.hurt(victim, damageSource, endlessDamage ? Float.MAX_VALUE : this.getTier().getAttackDamageBonus());
                 }
 
             } else {
-                victim.setInvulnerable(false);
-                victim.hurt(damageSource, endlessDamage ? Float.MAX_VALUE : this.getTier().getAttackDamageBonus());
+                this.hurt(victim, damageSource, endlessDamage ? Float.MAX_VALUE : this.getTier().getAttackDamageBonus());
             }
 
             if (endlessDamage) {
-                if (victim.isAlive() && victim instanceof LivingEntity livingEntity) {
-                    livingEntity.kill();//修正死亡
-                    livingEntity.dropAllDeathLoot(damageSource);
-                    player.killedEntity(serverLevel, livingEntity);
-                    serverLevel.broadcastEntityEvent(livingEntity, (byte)3);
+                if (victim.isDeadOrDying()) {
+                    victim.setHealth(0);//设置血量为零
+                    this.die(victim, damageSource);//修正设置死亡
+                    player.killedEntity(serverLevel, victim);//添加至信息统计
+                    //player.getCombatTracker().recordDamage(damageSource, victim.getHealth());//添加至伤害记录
                 }
             }
+            return true;
         }
-        victim.setPose(Pose.DYING);
-        return true;
+        return false;
     }
 
-//    @Override
-//    public boolean hurtEnemy(@NotNull ItemStack stack, @NotNull LivingEntity victim, LivingEntity livingEntity) {
-//        var level = livingEntity.level();
-//        var endlessDamage = ModConfig.isSwordAttackEndless.get();
-//        if (!level.isClientSide && livingEntity instanceof Player player && level instanceof ServerLevel serverLevel) {
-//            var damageSource = player.damageSources().source(ModDamageTypes.INFINITY, livingEntity, victim);
-//            ToolUtils.sweepAttack(level, livingEntity, victim);//横扫
-//            if (victim instanceof EnderDragon dragon ) {
-//                victim.setInvulnerable(false);//取消无敌
-//                dragon.hurt(dragon.head, damageSource, endlessDamage ? Float.MAX_VALUE : this.getTier().getAttackDamageBonus());
-//            } else if (victim instanceof Player pvp) {
-//                if (ToolUtils.isInfinite(pvp)) {
-//                    // 玩家身着无尽甲则只造成爆炸伤害
-//                    pvp.level().explode(livingEntity, pvp.getBlockX(), pvp.getBlockY(), pvp.getBlockZ(), 25.0F, Level.ExplosionInteraction.MOB);
-//                    return true;//直接返回
-//                } else {
-//                    victim.setInvulnerable(false);
-//                    victim.hurt(damageSource, endlessDamage ? Float.MAX_VALUE : this.getTier().getAttackDamageBonus());
-//                }
-//
-//            } else {
-//                victim.setInvulnerable(false);
-//                victim.hurt(damageSource, endlessDamage ? Float.MAX_VALUE : this.getTier().getAttackDamageBonus());
-//            }
-//
-//            victim.lastHurtByPlayerTime = 60;
-//            victim.getCombatTracker().recordDamage(damageSource, victim.getHealth());
-//
-//
-//            if (endlessDamage) {
-//                victim.setHealth(0);//设置血量为零
-//                victim.die(damageSource);//设置死亡
-//                if (victim.isAlive()) {
-//                    livingEntity.kill();//修正死亡
-//                    livingEntity.dropAllDeathLoot(damageSource);
-//                    player.killedEntity(serverLevel, victim);
-//                    serverLevel.broadcastEntityEvent(victim, (byte)3);
-//                }
-//            }
-//        }
-//        victim.setPose(Pose.DYING);
-//        return true;
-//    }
+    public boolean hurt(LivingEntity victim, DamageSource pSource, float pAmount) {
+        if (victim.level().isClientSide) {
+            return false;
+        } else if (victim.isDeadOrDying()) {
+            return false;
+        } else {
+            if (victim.isSleeping() && !victim.level().isClientSide) {
+                victim.stopSleeping();
+            }
 
+            boolean flag = false;
+
+            victim.setNoActionTime(0);
+            victim.walkAnimation.setSpeed(1.5F);
+            victim.lastHurt = pAmount;
+            victim.invulnerableTime = 20;
+            victim.getCombatTracker().recordDamage(pSource, pAmount);
+            victim.setHealth(victim.getHealth() - pAmount);
+            victim.gameEvent(GameEvent.ENTITY_DAMAGE);
+            victim.hurtDuration = 10;
+            victim.hurtTime = victim.hurtDuration;
+
+
+
+            Entity entity1 = pSource.getEntity();
+            if (entity1 != null) {
+                if (entity1 instanceof LivingEntity livingentity1) {
+                    if (!pSource.is(DamageTypeTags.NO_ANGER)) {
+                        victim.setLastHurtByMob(livingentity1);
+                    }
+                }
+
+                if (entity1 instanceof Player player1) {
+                    victim.lastHurtByPlayerTime = 100;
+                    victim.setLastHurtByPlayer(player1);
+                } else if (entity1 instanceof net.minecraft.world.entity.TamableAnimal tamableEntity) {
+                    if (tamableEntity.isTame()) {
+                        victim.lastHurtByPlayerTime = 100;
+                        LivingEntity livingentity2 = tamableEntity.getOwner();
+                        if (livingentity2 instanceof Player player2) {
+                            victim.setLastHurtByPlayer(player2);
+                        } else {
+                            victim.setLastHurtByPlayer(null);
+                        }
+                    }
+                }
+            }
+
+            victim.level().broadcastDamageEvent(victim, pSource);
+
+            if (!pSource.is(DamageTypeTags.NO_IMPACT)) {
+                victim.hurtMarked = true;
+            }
+
+            if (entity1 != null && !pSource.is(DamageTypeTags.IS_EXPLOSION)) {
+                double d0 = entity1.getX() - victim.getX();
+
+                double d1;
+                for(d1 = entity1.getZ() - victim.getZ(); d0 * d0 + d1 * d1 < 1.0E-4D; d1 = (Math.random() - Math.random()) * 0.01D) {
+                    d0 = (Math.random() - Math.random()) * 0.01D;
+                }
+
+                victim.knockback(0.4F, d0, d1);
+                if (!flag) {
+                    victim.indicateDamage(d0, d1);
+                }
+            }
+
+            if (victim.isDeadOrDying()) {
+                this.die(victim, pSource);
+            } else {
+                SoundEvent soundevent = SoundEvents.GENERIC_HURT;
+                victim.playSound(soundevent, 2F, victim.getVoicePitch());
+            }
+
+            boolean flag2 = true;
+            victim.lastDamageSource = pSource;
+            victim.lastDamageStamp = victim.level().getGameTime();
+
+            if (victim instanceof ServerPlayer) {
+                CriteriaTriggers.ENTITY_HURT_PLAYER.trigger((ServerPlayer)victim, pSource, pAmount, pAmount, flag);
+            }
+
+            if (entity1 instanceof ServerPlayer) {
+                CriteriaTriggers.PLAYER_HURT_ENTITY.trigger((ServerPlayer)entity1, victim, pSource, pAmount, pAmount, flag);
+            }
+
+            return flag2;
+        }
+    }
+
+    public void die(LivingEntity victim, DamageSource pDamageSource) {
+        if (net.minecraftforge.common.ForgeHooks.onLivingDeath(victim, pDamageSource)) return;
+        if (!victim.isRemoved() && !victim.dead) {
+            Entity entity = pDamageSource.getEntity();
+            LivingEntity livingentity = victim.getKillCredit();
+            if (victim.deathScore >= 0 && livingentity != null) {
+                livingentity.awardKillScore(victim, victim.deathScore, pDamageSource);
+            }
+
+            if (victim.isSleeping()) {
+                victim.stopSleeping();
+            }
+
+            if (!victim.level().isClientSide && victim.hasCustomName()) {
+                Static.LOGGER.info("Named entity {} died: {}", this, victim.getCombatTracker().getDeathMessage().getString());
+            }
+
+            victim.dead = true;
+            victim.getCombatTracker().recheckStatus();
+            Level level = victim.level();
+            if (level instanceof ServerLevel serverlevel) {
+                if (entity == null || entity.killedEntity(serverlevel, victim)) {
+                    victim.gameEvent(GameEvent.ENTITY_DIE);
+                    victim.dropAllDeathLoot(pDamageSource);
+                    this.createWitherRose(victim, livingentity);
+                }
+
+                victim.level().broadcastEntityEvent(victim, (byte)3);
+            }
+
+            victim.setPose(Pose.DYING);
+        }
+    }
+
+    protected void createWitherRose(LivingEntity victim, @Nullable LivingEntity pEntitySource) {
+        if (!victim.level().isClientSide) {
+            boolean flag = false;
+            if (pEntitySource instanceof WitherBoss) {
+                if (net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent(victim.level(), pEntitySource)) {
+                    BlockPos blockpos = victim.blockPosition();
+                    BlockState blockstate = Blocks.WITHER_ROSE.defaultBlockState();
+                    if (victim.level().isEmptyBlock(blockpos) && blockstate.canSurvive(victim.level(), blockpos)) {
+                        victim.level().setBlock(blockpos, blockstate, 3);
+                        flag = true;
+                    }
+                }
+
+                if (!flag) {
+                    ItemEntity itementity = new ItemEntity(victim.level(), victim.getX(), victim.getY(), victim.getZ(), new ItemStack(Items.WITHER_ROSE));
+                    victim.level().addFreshEntity(itementity);
+                }
+            }
+
+        }
+    }
     @Override
     public @NotNull InteractionResultHolder<ItemStack> use(Level level, Player player, @NotNull InteractionHand hand) {
         var heldItem = player.getItemInHand(hand);
